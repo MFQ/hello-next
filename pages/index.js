26aa69b46617db3886d6ed68f4f17ff5c0467b1e
Muhammad Fazil Qureshi <softmfq@gmail.com>
2018-04-07 23:25:19 +0500

    Create dynamic links

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
 import Layout from '../components/Layout';
+import Link from 'next/link';
+
+const PostLink = (props) => (
+  <li>
+    <Link href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
+)
 
 const Index = () => (
   <div>
     <Layout>
-      <p>Hello Next.js</p>
+      <h1> My Blog </h1>
+      <PostLink title='Hello Next.js' />
+      <PostLink title='Learn Next.js is awesome' />
+      <PostLink title='Deploy apps with Zeit' />
     </Layout>
   </div>
 )
